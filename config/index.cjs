77eed520ca6550a80ecf224ad827a29cc1919537
lhrLoveTx <70141081+lhrLoveTx@users.noreply.github.com>
2022-11-23 19:08:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '傻丫头',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oJ_ZI64YuXlcRo_9yAT9R',
+      id: 'oJ_ZI64YuXlcRo_9yAT9R-rAfy5Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '7qEHIyMkTEe09nfwWKPmyYondosP2e4OTGmnR_YjfVA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'oJ_ZI64YuXlcRo_9yAT9R-rAfy5Q',
+  CALLBACK_TEMPLATE_ID: '7qEHIyMkTEe09nfwWKPmyYondosP2e4OTGmnR_YjfVA',
 
   CALLBACK_USERS: [
     {
